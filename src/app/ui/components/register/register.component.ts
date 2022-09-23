735f6fd2950162ac9d1bdae0a647ba20cebcfc53
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-09-23 13:48:15 +0300

    23.09 / Kullanıcı Kayıt Formu Tasarlama (Reactive Form) -2

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, TRANSLATIONS } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,15 +13,49 @@ export class RegisterComponent implements OnInit {
   frm:FormGroup;
   ngOnInit(): void {
     this.frm=this.formBuilder.group({
-      adSoyad:[""],
-      kullaniciAdi:[""],
-      email:[""],
-      sifre:[""],
-      sifreTekrar:[""]
+      adSoyad:["", [
+        Validators.required,
+        Validators.maxLength(50),
+        Validators.minLength(2)
+      ]],
+      kullaniciAdi:["", [
+        Validators.required,
+        Validators.maxLength(500),
+        Validators.minLength(2)
+      ]],
+      email:["", [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(500),
+        Validators.minLength(2)
+      ]],
+      sifre:["",
+    [
+      Validators.required
+    ]],
+      sifreTekrar:["",
+    [
+      Validators.required
+    ]]
+    },
+    {
+      validator:(group:AbstractControl):ValidationErrors | null =>{
+        let sifre = group.get("sifre").value;
+        let sifreTekrar = group.get("sifreTekrar").value; 
+        return sifre==sifreTekrar ? null :{notSame:true};
+      }
     })
   }
 
+  get component(){
+    return this.frm.controls;
+  }
+
+  submitted:boolean=false;
   onSubmit(data:any){
+    this.submitted=true;
+    if(this.frm.invalid)
+    return;
 debugger;
   }
 
