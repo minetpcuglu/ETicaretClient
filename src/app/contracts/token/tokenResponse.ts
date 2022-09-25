35d76522bb67db6cbe5f716bab315b217a05cebc
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-09-25 21:41:46 +0300

    25.09 / JWT implementasyonu

---
+++ src/app/contracts/token/tokenResponse.ts
@@ -0,0 +1,6 @@
+
+import { AccessToken } from "./accesstoken";
+
+export class TokenResponse{
+    token:AccessToken
+}
--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,15 +1,18 @@
-import { HttpErrorResponse } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
+import { AccessToken } from 'src/app/contracts/token/accesstoken';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
 import { CreateUser } from 'src/app/contracts/users/createuser';
 import { User } from 'src/app/entities/user';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private httpClientService:HttpClientService) { }
+  constructor(private httpClientService:HttpClientService,private toastrService:CustomToastrService) { }
   async create(user:User):Promise<CreateUser>{
     const observable:Observable<CreateUser|User>=this.httpClientService.post<CreateUser|User>({
       controller:"users",
@@ -19,12 +22,22 @@ export class UserService {
     return await firstValueFrom(observable) as CreateUser;
   }
 
-  async login(usernameOrEmail:string,password:string,callBackFunction?:()=>void):Promise<void>{
-    const observable:Observable<any>=this.httpClientService.post({
+  async login(usernameOrEmail:string,password:string,callBackFunction?:()=>void):Promise<any>{
+    const observable:Observable<any|TokenResponse>=this.httpClientService.post<any|TokenResponse>({
       controller:"users",
       action:"login"
     },{usernameOrEmail,password})
-    await firstValueFrom(observable);
+    const tokenResponse:TokenResponse= await firstValueFrom(observable) as TokenResponse;
+    if(tokenResponse)
+    {
+      localStorage.setItem("token",tokenResponse.token.token);
+      // localStorage.setItem("expiration",accesstoken.expiration.toString());
+      this.toastrService.message("Kullanıcı girişi başarılı","giriş başarılı",{
+        position:ToastrPosition.TopRigth,
+        messageType:ToastrMessageType.Success
+      });
+    }
+   
     callBackFunction();
   }
 }
